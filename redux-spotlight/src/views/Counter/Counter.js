b188d5c9f8011972da1dafbc8803d363e4b7c601
spencer-eagleton <86867967+spencer-eagleton@users.noreply.github.com>
2022-02-15 11:30:28 -0800

    added multiply by two button and dispatch

--- redux-spotlight/src/views/Counter/Counter.js
+++ redux-spotlight/src/views/Counter/Counter.js
@@ -1,33 +1,54 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { decrement, increment } from './counterSlice';
+import { decrement, increment, multiply, darkMode } from './counterSlice';
 
 const Counter = () => {
   // The function below is called a selector and allows us to select a value from
   // the state.
   const count = useSelector((state) => state.counter.count);
+  const darkMode = useSelector((state) => state.counter.darkmode);
   const dispatch = useDispatch();
 
   return (
-    <div>
-      <button
-        aria-label="Decrement value"
-        onClick={() => {
-          dispatch(decrement());
-        }}
-      >
-        -
-      </button>
-      <span>{count}</span>
-      <button
-        aria-label="Increment value"
-        onClick={() => {
-          dispatch(increment());
-        }}
-      >
-        +
-      </button>
-    </div>
+    <>
+      <div>
+        <button
+          aria-label="darkmode"
+          onClick={() => {
+            dispatch(darkMode());
+          }}
+        >
+          darkmode
+        </button>
+      </div>
+      <div>
+        <button
+          aria-label="Decrement value"
+          onClick={() => {
+            dispatch(decrement());
+          }}
+        >
+          -
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Increment value"
+          onClick={() => {
+            dispatch(increment());
+          }}
+        >
+          +
+        </button>
+        <button
+          aria-label="multiply value"
+          onClick={() => {
+            dispatch(multiply());
+          }}
+        >
+          *2
+        </button>
+      </div>
+    </>
   );
 };
 
--- redux-spotlight/src/views/Counter/counterSlice.js
+++ redux-spotlight/src/views/Counter/counterSlice.js
@@ -2,6 +2,7 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   count: 0,
+  darkmode: false,
 };
 
 export const counterSlice = createSlice({
@@ -17,10 +18,17 @@ export const counterSlice = createSlice({
     decrement: (state) => {
       state.count -= 1;
     },
+    multiply: (state) => {
+      state.count *= 2;
+    },
+    darkMode: (state) => {
+      state.darkmode = !state.darkmode;
+    },
   },
 });
 
-export const { increment, decrement } = counterSlice.actions;
+export const { increment, decrement, multiply, darkMode } =
+  counterSlice.actions;
 
 //Export the reducer function from the slice
 export default counterSlice.reducer;
